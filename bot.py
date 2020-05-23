916886de8a0b71d4fcaca5b64d6e8f392f689bbe
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2020-05-23 10:37:12 +0800

    style updates

--- bot.py
+++ bot.py
@@ -7,21 +7,31 @@ import os
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix= 'c!')
+bot = commands.Bot(command_prefix= '.')
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+    
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(509693136247848961)
+    await channel.send(f"**{member}**歡迎喵~")
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(509693136247848961)
+    await channel.send(f"抓到了喵!!**{member}**在偷尻喵~")
 
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cmds.{extension}')
-    await ctx.send(f'Loaded {extension} done.')
+    await ctx.send(f'學習了 **{extension}** 指令包!!奇怪的知識增加了喵~')
 
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'Un - Loaded {extension} done.')
+    await ctx.send(f'少了 **{extension}** 指令包，本喵現在覺得一身輕喵~')
 
 @bot.command()
 async def reload(ctx, extension):
--- cmds/common.py
+++ cmds/common.py
@@ -7,7 +7,7 @@ class Common(Cog_Extension):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
+        await ctx.send(f'本喵現在的反應延遲是 **{round(self.bot.latency*1000)}** 毫秒，至少比你的頭腦還要快上 **{round(250/(self.bot.latency*1000),3)}** 倍喵(・∀・)')
 
 
 def setup(bot):
