c4d0054a785e7b97c140ac799d55c98c253f3992
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2020-06-21 10:39:59 +0800

    exception handle updates
    1.exception handle
    2.remove help.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import os
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
+
 bot = commands.Bot(command_prefix= '.')
 
 @bot.event
@@ -45,15 +46,58 @@ async def reload(ctx, extension):
 
 bot.remove_command('help')
 @bot.command()
-async def help(ctx):
-    embed=discord.Embed(title="桔喵小助手", description="指令查詢", color=0xffe26f)
-    embed.set_thumbnail(url="https://i.imgur.com/pms8YGV.png")
-    embed.add_field(name="Arcaea", value="arc | arcbomb", inline=False)
-    embed.add_field(name="Common", value="member | mj | ping | say", inline=False)
-    embed.add_field(name="Random", value="choose | mora | rs", inline=False)
-    embed.set_footer(text="使用.cmd來查詢你想了解的指令")
-    await ctx.send(embed=embed)
+async def help(ctx, *comad):
+    if comad:
+        embed=discord.Embed(title="指令查詢", color=0xffe26f)
+        if comad[0] == 'arc':
+            embed.add_field(name="Arcaea", value=".arc <amount> 抽取隨機Arcaea歌曲", inline=False)
+        elif comad[0] == 'arcbomb':
+            embed.add_field(name="Arcaea", value=".arcbomb <amount> [difficulty...] 連續抽取2~5首Arcaea歌曲", inline=False)
+        elif comad[0] == 'member':
+            embed.add_field(name="Common", value=".member 顯示當前群組人數", inline=False)
+        elif comad[0] == 'mj':
+            embed.add_field(name="Common", value=".mj <name> 查詢成員進入伺服器時間", inline=False)
+        elif comad[0] == 'ping':
+            embed.add_field(name="Common", value=".ping 查詢現在bot的延遲", inline=False)
+        elif comad[0] == 'say':
+            embed.add_field(name="Common", value=".say <msg> 讓bot重複訊息", inline=False)
+        elif comad[0] == 'choose':
+            embed.add_field(name="Random", value=".choose [anything...]隨機選擇輸入的文字", inline=False)
+        elif comad[0] == 'mora':
+            embed.add_field(name="Random", value=".mora 猜拳", inline=False)
+        elif comad[0] == 'rs':
+            embed.add_field(name="Random", value=".rs <num_of_people> <group_amount> <role> 隨機分隊", inline=False)
+        else:
+            embed.add_field(name="Not Found", value="查無此指令", inline=False)
+    
+        await ctx.send(embed=embed)    
+        
+    else:   
+        embed=discord.Embed(title="桔喵小助手", description="指令查詢", color=0xffe26f)
+        embed.set_thumbnail(url="https://i.imgur.com/pms8YGV.png")
+        embed.add_field(name="Arcaea", value="arc | arcbomb", inline=False)
+        embed.add_field(name="Common", value="member | mj | ping | say", inline=False)
+        embed.add_field(name="Random", value="choose | mora | rs", inline=False)
+        embed.set_footer(text="使用.help <cmd>來查詢你想了解的指令")
+        await ctx.send(embed=embed)
 
+@bot.event
+async def on_command_error(ctx, error):
+    embed=discord.Embed(title="ERROR", color=0xffe26f)
+
+    if isinstance(error, commands.errors.CommandNotFound):
+        embed.add_field(name="指令錯誤", value="請不要講一些本喵聽不懂的話喵！", inline=False)
+    elif isinstance(error, commands.errors.MissingRequiredArgument):
+        embed.add_field(name="缺少必要參數", value="你什麼都不說是要本喵怎麼辦喵?", inline=False)
+    elif isinstance(error, commands.errors.BadArgument):
+        embed.add_field(name="型態錯誤或不明的參數", value="找遍了本喵的資料庫也幫不了你了喵~", inline=False)
+    elif isinstance(error, commands.errors.CommandInvokeError):
+        embed.add_field(name="呼叫指令時出現例外狀況", value="你...你...你...罰你去看.help喵!!", inline=False)
+    else:
+        embed.add_field(name="發生錯誤", value="本喵什麼都不知道喵~", inline=False)
+
+    embed.set_footer(text="使用.help <cmd>來查詢你想了解的指令")
+    await ctx.send(embed=embed)
 
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
