95909318ca09689964f81bc18a28359e75649403
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2020-08-05 23:17:22 +0800

    ff

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def count(c):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status = discord.Status.do_not_disturb, activity=discord.Game('.help → get help'))
+    await bot.change_presence(status = discord.Status.online, activity=discord.Game('.help → get help'))
     print(">> Bot is online <<")
     
 @bot.event
