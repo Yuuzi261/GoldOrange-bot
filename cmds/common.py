fc888e66018011c69c3532b30e0510fe37645239
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2020-08-21 16:13:50 +0800

    Arcaea songs update to v3.0 full

--- cmds/common.py
+++ cmds/common.py
@@ -45,7 +45,7 @@ class Common(Cog_Extension):
         embed.set_thumbnail(url="https://i.imgur.com/pms8YGV.png")
         embed.add_field(name="**本喵的主要開發者是 金桔 喵~**", value="目前沒有其它的共同開發者喵！\n特別感謝以下的人協助開發喵(ㆁωㆁ)", inline=False)
         embed.add_field(name="**:small_orange_diamond: Proladon以及其他所有SHELTER ZONE的大佬**", value="感謝協助Debug以及提供教學喵", inline=False)
-        embed.add_field(name="**:small_orange_diamond: SJ**", value="感謝提供C2所有歌曲圖片喵", inline=False)
+        embed.add_field(name="**:small_orange_diamond: SJ**", value="感謝提供C2、Deemo所有歌曲圖片喵", inline=False)
         embed.add_field(name="**:small_orange_diamond: 小杰**", value="感謝提供arcaea所有歌曲圖片喵", inline=False)
         embed.add_field(name="**:small_orange_diamond: 所有參與測試人員**", value="感謝所有群內幫助測試以及提供意見的人員喵", inline=True)
         embed.set_footer(text="Programmer : 金桔")
